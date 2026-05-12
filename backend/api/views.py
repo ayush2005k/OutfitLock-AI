566f8fa67b4e9feccba0918bbba4e100a9826fc9
Ayush Singh <ayushsingh100k@gmail.com>
2026-05-13 00:15:00 +0530

    created file upload api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django.conf import settings
+
+
+@api_view(['POST'])
+def generate_images(request):
+
+    outfits = request.FILES.getlist('outfits')
+
+    references = request.FILES.getlist('references')
+
+    uploaded_files = []
+
+    # SAVE OUTFITS
+    for file in outfits:
+
+        file_path = os.path.join(
+            settings.MEDIA_ROOT,
+            file.name
+        )
+
+        with open(file_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        uploaded_files.append(file.name)
+
+    return Response({
+        "message": "Files uploaded",
+        "files": uploaded_files
+    })
