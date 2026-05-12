1a323b40a9280e2948b8d827d7045ae292701709
Ayush Singh <ayushsingh100k@gmail.com>
2026-05-13 00:21:35 +0530

    code modification

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,35 +1,10 @@
-import os
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from django.conf import settings
 
-
-@api_view(['POST'])
+@api_view(['GET'])
 def generate_images(request):
 
-    outfits = request.FILES.getlist('outfits')
-
-    references = request.FILES.getlist('references')
-
-    uploaded_files = []
-
-    # SAVE OUTFITS
-    for file in outfits:
-
-        file_path = os.path.join(
-            settings.MEDIA_ROOT,
-            file.name
-        )
-
-        with open(file_path, 'wb+') as destination:
-            for chunk in file.chunks():
-                destination.write(chunk)
-
-        uploaded_files.append(file.name)
-
     return Response({
-        "message": "Files uploaded",
-        "files": uploaded_files
+        "message": "Backend working successfully"
     })
