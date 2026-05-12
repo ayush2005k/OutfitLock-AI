e33c7fb47aaeec866b5f1714b5f57828139a265d
Ayush Singh <ayushsingh100k@gmail.com>
2026-05-13 00:13:42 +0530

    database created

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Generation(models.Model):
+
+    outfit_name = models.CharField(max_length=255)
+
+    generated_image = models.ImageField(
+        upload_to='outputs/'
+    )
+
+    status = models.CharField(
+        max_length=50,
+        default='generated'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.outfit_name
